c345d060e6ec22a601540f0fd356d26d3ccbbbbd
Andrew Pobrica <pirit66@gmail.com>
2021-08-21 21:51:05 -0400

    Appears to be a meh bot, still fails one check, moves to an invalid location

--- p01.py
+++ p01.py
@@ -1,4 +1,5 @@
 from rgkit import rg
+from random import choices
 
 
 class Robot:
@@ -10,6 +11,62 @@ class Robot:
     ['guard']
     ['suicide']
     """
+    val = ""
 
+    # file = open("log.txt", 'w')
     def act(self, game):
+
+        if 'spawn' in rg.loc_types(self.location):
+            if rg.locs_around(self.location, filter_out=('invalid', 'spawn', 'obstacle')):
+                print("move 5", game.robots[self.location])
+                # print(rg.locs_around(self.location, filter_out=('invalid', 'spawn', 'obstacle')))
+                return ['move', rg.locs_around(self.location, filter_out=('invalid', 'spawn', 'obstacle'))[0]]
+            elif rg.locs_around(self.location, filter_out=('invalid', 'obstacle')):
+                print("move 6", game.robots[self.location])
+                return ['move', rg.locs_around(self.location, filter_out=('invalid', 'obstacle'))[0]]
+
+
+        directions = []
+        nearby_enemies_count = 0
+        nearby_enemies = []
+        for loc, bot in game.robots.items():
+            # print(str(bot))
+            if bot.player_id != self.player_id:
+                if rg.wdist(loc, self.location) <= 1:
+                    nearby_enemies_count += 1
+                    nearby_enemies += (loc, bot)
+        #print(nearby_enemies)
+        if nearby_enemies_count:
+            if self.hp < 9 * nearby_enemies_count:
+                return ['suicide']
+            else:
+                return ['attack', nearby_enemies[0]]
+
+        t = rg.CENTER_POINT
+        x = 99
+
+        for loc, bot in game.robots.items():
+            #print(game.robots.items())
+            if bot.player_id != self.player_id and rg.wdist(loc, self.location) < x:
+                #print(x)
+                t = loc
+                x = rg.wdist(loc, self.location)
+        #print(rg.toward(self.location, t))
+
+        if 'obstacle' not in rg.loc_types(rg.toward(self.location, t)) \
+                and 'invalid' not in rg.loc_types(rg.toward(self.location, t)) \
+                and 'spawn' not in rg.loc_types(rg.toward(self.location, t)) and x < 19:
+            print("move 1", game.robots[self.location])
+            return ['move', (rg.toward(self.location, t))]
+
+        elif 'obstacle' not in rg.loc_types(rg.toward(self.location, rg.CENTER_POINT)) \
+                and 'invalid' not in rg.loc_types(rg.toward(self.location, rg.CENTER_POINT)) \
+                and 'spawn' not in rg.loc_types(rg.toward(self.location, rg.CENTER_POINT)):
+            print("move 2", game.robots[self.location])
+            return ['move', rg.toward(self.location, rg.CENTER_POINT)]
+
+        elif rg.locs_around(self.location, filter_out=('invalid', 'spawn', 'obstacle')):
+            print("move 3", game.robots[self.location])
+            return ['move', choices(rg.locs_around(self.location, filter_out=('invalid', 'spawn', 'obstacle')))[0]]
+
         return ['guard']
