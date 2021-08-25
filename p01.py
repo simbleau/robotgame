e50cd226edcec16c10ca29f904c965656dcd1f94
Matt Sterckx <sterckx.matthias@live.com>
2021-08-25 02:32:37 -0400

    Started optimizing and rewriting code

--- p01.py
+++ p01.py
@@ -40,23 +40,20 @@ def urgency(bot1):
 def greater(bot1, bot2):
     if urgency(bot1) > urgency(bot2):
         return 1
-    if urgency(bot2) > urgency(bot1):
-        return 0
+#    if urgency(bot2) > urgency(bot1):
+    return 0
     # deliberately runs off the edge; this should be impossible.
 
 
 def valid(move):
     types = rg.loc_types(move)
-    if 'invalid' in types:
-        return 0
-    if 'obstacle' in types:
+    if 'invalid' in types or 'obstacle' in types:
         return 0
     return 1
 
 
 def not_spawn(move):
-    types = rg.loc_types(move)
-    if 'spawn' in types:
+    if 'spawn' in rg.loc_types(move):
         return 0
     return 1
 
@@ -66,7 +63,7 @@ def spawn(move):
 
 
 def surrounded_spawn(move):
-    for _ in rg.locs_around(move, filter_out=('obstacle', 'invalid', 'spawn')):
+    if len(rg.locs_around(move, filter_out=('obstacle', 'invalid', 'spawn'))) > 0:
         return 0
     return 1
 
@@ -80,10 +77,8 @@ def equal(bot1, bot2):
 def surrounders(this_robot, game, loc):
     number_found = 0
     for loc2 in rg.locs_around(loc):
-        if loc2 in game.robots:
-            bot2 = game.robots[loc2]
-            if bot2.player_id != this_robot.player_id:
-                number_found += 1
+        if loc2 in game.robots and game.robots[loc2].player_id != this_robot.player_id:
+            number_found += 1
     return number_found
 
 
@@ -110,69 +105,45 @@ def move_towards_if_no_ally(loc, dest, game, illegals):
     xmove = towardsx_if_not_spawn(loc, dest)
     ymove = towardsy_if_not_spawn(loc, dest)
     if xmove != 'no_move' and not (xmove in illegals) and not (xmove in game.robots):
-        xvalid = 1
-    else:
-        xvalid = 0
-    if ymove != 'no_move' and not (ymove in illegals) and not (ymove in game.robots):
-        yvalid = 1
-    else:
-        yvalid = 0
-    if xvalid == 1:
         return xmove
-    if yvalid == 1:
+    if ymove != 'no_move' and not (ymove in illegals) and not (ymove in game.robots):
         return ymove
     return 'no_action'
 
 
 def towardsy_if_not_spawn(loc, dest):
-    if dest[1] > loc[1]:
-        tentative_move = (loc[0], loc[1] + 1)
-        if valid(tentative_move):
-            if not_spawn(tentative_move) or spawn(loc):
-                return tentative_move
+    tentative_move = (loc[0], loc[1] + 1)
     if dest[1] < loc[1]:
         tentative_move = (loc[0], loc[1] - 1)
-        if valid(tentative_move):
-            if not_spawn(tentative_move) or spawn(loc):
-                return tentative_move
+    if dest[1] != loc[1] and valid(tentative_move) and (not_spawn(tentative_move) or spawn(loc)):
+        return tentative_move
     return 'no_move'
 
 
 def towardsx_if_not_spawn(loc, dest):
-    if dest[0] > loc[0]:
-        tentative_move = (loc[0] + 1, loc[1])
-        if valid(tentative_move):
-            if not_spawn(tentative_move) or not not_spawn(loc):
-                return tentative_move
+    tentative_move = (loc[0] + 1, loc[1])
     if dest[0] < loc[0]:
         tentative_move = (loc[0] - 1, loc[1])
-        if valid(tentative_move):
-            if not_spawn(tentative_move) or not not_spawn(loc):
-                return tentative_move
+    if dest[0] != loc[0] and valid(tentative_move) and (not_spawn(tentative_move) or spawn(loc)):
+        return tentative_move
     return 'no_move'
 
 
 def towardy(loc, dest):
-    if dest[1] > loc[1]:
-        tentative_move = (loc[0], loc[1] + 1)
-        if valid(tentative_move):
-            return tentative_move
+    tentative_move = (loc[0], loc[1] + 1)
     if dest[1] < loc[1]:
         tentative_move = (loc[0], loc[1] - 1)
-        if valid(tentative_move):
-            return tentative_move
+    if dest[1] != loc[1] and valid(tentative_move):
+        return tentative_move
     return 'no_move'
 
 
 def towardx(loc, dest):
-    if dest[0] > loc[0]:
-        tentative_move = (loc[0] + 1, loc[1])
-        if valid(tentative_move):
-            return tentative_move
+    tentative_move = (loc[0] + 1, loc[1])
     if dest[0] < loc[0]:
         tentative_move = (loc[0] - 1, loc[1])
-        if valid(tentative_move):
-            return tentative_move
+    if dest[0] != loc[0] and valid(tentative_move):
+        return tentative_move
     return 'no_move'
 
 
@@ -190,11 +161,9 @@ def move_towards_either_axis(loc, dest, turn):
 
 
 def destruct_if_doomed_us(this_robot, game, illegals):
-    if this_robot.location in illegals:
+    if this_robot.location in illegals or surrounders(this_robot, game, this_robot.location) * suicide_param <= this_robot.hp:
         return 'no_action'
-    if surrounders(this_robot, game, this_robot.location) * suicide_param > this_robot.hp:
-        return ['suicide']
-    return 'no_action'
+    return ['suicide']
 
 
 def destruct_if_doomed_enemy(this_robot, game):
@@ -261,56 +230,42 @@ def strong_hunt_the_weak(this_robot, game, illegals):
                     best_move = ['attack', bot.location]
                     weakest_enemy = bot.hp
     for bot in two_robots:
-        if bot.player_id != this_robot.player_id:
-            if bot.hp < weakest_enemy:
-                targetx = towardsx_if_not_spawn(this_robot.location, bot.location)
-                targety = towardsy_if_not_spawn(this_robot.location, bot.location)
-                if not (targetx == 'no_move'):
-                    if not (targetx in illegals or surrounders(this_robot, game, targetx) > 1):
-                        best_move = ['move', targetx]
-                        weakest_enemy = bot.hp
-                if not (targety == 'no_move'):
-                    if not (targety in illegals or surrounders(this_robot, game, targety) > 1):
-                        if targetx == 'no_move' or rg.dist(targetx, rg.CENTER_POINT) > rg.dist(targety,
-                                                                                               rg.CENTER_POINT):
-                            best_move = ['move', targety]
-                            weakest_enemy = bot.hp
+        if bot.player_id != this_robot.player_id and bot.hp < weakest_enemy:
+            targetx = towardsx_if_not_spawn(this_robot.location, bot.location)
+            targety = towardsy_if_not_spawn(this_robot.location, bot.location)
+            if targetx != 'no_move' and not (targetx in illegals or surrounders(this_robot, game, targetx) > 1):
+                best_move = ['move', targetx]
+                weakest_enemy = bot.hp
+            if targety != 'no_move' and not (targety in illegals or surrounders(this_robot, game, targety) > 1):
+                if targetx == 'no_move' or rg.dist(targetx, rg.CENTER_POINT) > rg.dist(targety, rg.CENTER_POINT):
+                    best_move = ['move', targety]
+                    weakest_enemy = bot.hp
     return best_move
 
 
 def safe(this_robot, loc, game):
-    turns_left = 10 - game.turn % 10
-    if turns_left == 10:
-        turns_left = 0
+    turns_left = (10 - game.turn) % 10
     if turns_left <= 2 and spawn(loc):
         return 0
     for bot in one_robots:
         if loc == bot.location and bot.player_id != this_robot.player_id:
             return 0
     for bot in two_robots:
-        if bot.player_id != this_robot.player_id:
-            if rg.wdist(loc, bot.location) == 1:
-                return 0
+        if bot.player_id != this_robot.player_id and rg.wdist(loc, bot.location) == 1:
+            return 0
     return 1
 
 
 def scared(this_robot, game):
     num_surrounders = 0
-    is_scared = 0
-    hp = 0
     for bot in one_robots:
         if bot.player_id != this_robot.player_id:
             num_surrounders += 1
-            hp = bot.hp
-            # last_found = bot
-            if destruct_if_doomed_enemy(bot, game) != 'no_action':
-                is_scared = 1
+            if destruct_if_doomed_enemy(bot, game) != 'no_action' or (bot.hp > this_robot.hp and (surrounders(bot, game, bot.location) == 1 or this_robot.hp < 16)):
+                return 1
     if num_surrounders > 1:
-        is_scared = 1
-    if hp > this_robot.hp:
-        if (surrounders(bot, game, bot.location) == 1) or this_robot.hp < 16:
-            is_scared = 1
-    return is_scared
+        return 1
+    return 0
 
 
 def run_if_scared_and_safe(this_robot, game, illegals):
@@ -319,10 +274,9 @@ def run_if_scared_and_safe(this_robot, game, illegals):
     best_distance = 1000
     move = 'no_action'
     for loc in rg.locs_around(this_robot.location, filter_out=('obstacle', 'invalid', 'spawn')):
-        if not (loc in illegals) and safe(this_robot, loc, game) == 1:
-            if rg.dist(loc, rg.CENTER_POINT) < best_distance:
-                best_distance = rg.dist(loc, rg.CENTER_POINT)
-                move = ['move', loc]
+        if not (loc in illegals) and safe(this_robot, loc, game) == 1 and rg.dist(loc, rg.CENTER_POINT) < best_distance:
+            best_distance = rg.dist(loc, rg.CENTER_POINT)
+            move = ['move', loc]
     return move
 
 
@@ -370,29 +324,20 @@ def find_empty_space(this_robot, game, illegals):
 
 
 def pin_to_spawn(this_robot, game, illegals):
-    if game.turn > 95:
-        return 'no_action'
-    turns_left = 10 - game.turn % 10
-    if turns_left == 10:
-        turns_left = 0
-    if this_robot.hp < hp_to_pin[turns_left]:
+    turns_left = (10 - game.turn) % 10
+    if game.turn > 95 or this_robot.hp < hp_to_pin[turns_left]:
         return 'no_action'
     loc = this_robot.location
     for bot in one_robots:
-        if bot.player_id != this_robot.player_id:
-            loc2 = bot.location
-            if spawn(loc2):
-                if not_spawn(loc) and not (loc in illegals):
-                    return ['guard']
+        if bot.player_id != this_robot.player_id and (spawn(bot.location) and not_spawn(loc) and not (loc in illegals)):
+            return ['guard']
     for bot in two_robots:
-        if bot.player_id != this_robot.player_id:
-            loc2 = bot.location
-            if spawn(loc2):
-                block_square = move_towards_either_axis(loc, loc2, game.turn)
-                if block_square == 'no_move':
-                    return 'no_action'
-                if not_spawn(block_square) and not (block_square in illegals):
-                    return ['move', block_square]
+        if bot.player_id != this_robot.player_id and spawn(bot.location):
+            block_square = move_towards_either_axis(loc, bot.location, game.turn)
+            if block_square == 'no_move':
+                return 'no_action'
+            if not_spawn(block_square) and not (block_square in illegals):
+                return ['move', block_square]
     return 'no_action'
 
 
